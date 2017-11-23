8ce050b97882dc34c1de07ea078bf141d1b42e91
isha <isha90.srivastava@gmail.com>
2017-11-23 23:21:17 +0530

    historicaldata feature added

--- src/historicaldata.py
+++ src/historicaldata.py
@@ -1,2 +1,16 @@
-def historicaldata(comindex):
-	print("FIX here")
+import utils
+import pandas as pd
+from datetime import datetime, timedelta, date
+
+def historicaldata(companylist,comindex):
+	ticker = companylist.iloc[int(comindex)].Symbol
+	company_details = pd.read_csv("https://www.google.com/finance/historical?output=csv&q={}".format(ticker))
+	step = timedelta(days=1)
+	df2 = pd.DataFrame()
+	from_dte,to_dte = utils.get_date()
+	while(from_dte <= to_dte):
+		from_date=datetime.strftime(from_dte, '%d-%b-%y')
+		df = company_details[company_details['Date'].str.match(from_date, case=False)]
+		df2 = df2.append(df)
+		from_dte += step
+	print(df2)
--- src/main.py
+++ src/main.py
@@ -10,10 +10,10 @@ from historicaldata import historicaldata
 from prediction import prediction
 import subprocess as sp
 
-def cls(): 
+def cls():
 	tmp = sp.call('cls',shell = True)
 
-def menu2(comindex):
+def menu2(companylist,comindex):
 	#cls()
 	loop = True
 	while loop:
@@ -26,16 +26,16 @@ def menu2(comindex):
 		print("6. Exit")
 		option = input("Your option: ")
 		if option == '1':
-			summary(comindex)
-		elif option == '2':	
-			graph(comindex),
-		elif option == '3':	
-			historicaldata(comindex),
-		elif option == '4':	
-			prediction(comindex),
-		elif option == '5':	
+			summary(companylist,comindex)
+		elif option == '2':
+			graph(companylist,comindex),
+		elif option == '3':
+			historicaldata(companylist,comindex),
+		elif option == '4':
+			prediction(companylist,comindex),
+		elif option == '5':
 			menu()
-		elif option == '6':	
+		elif option == '6':
 			print("Thanks for coming. Please visit again ...")
 			exit()
 		else:
@@ -43,7 +43,7 @@ def menu2(comindex):
 
 def name():
 	cls()
-	#TODO Name search is not working 
+	#TODO Name search is not working
 	companylist = pd.read_csv("http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchan0ge=nasdaq&render=download")
 	companyName = input("Enter the company name you want to search:")
 
@@ -59,12 +59,12 @@ def name():
 		outlen = len(companylist[companylist['Name'].str.match(companyName,case = False)])
 	print(companyName)
 	print(companylist[companylist['Name'].str.match(companyName,case = False)])
-	#print(companylist.dtypes)	
+	#print(companylist.dtypes)
 	comindex = input("Enter the index number of the company you want:")
 	print("You selected :")
 	print((companylist.iloc[int(comindex)]))
 	#print((companylist.loc[int(comindex)]))
-	menu2(comindex)
+	menu2(companylist,comindex)
 
 
 def ticker():
@@ -74,7 +74,7 @@ def ticker():
 
 	matchTicker = companylist[companylist['Symbol'].str.match(companyTicker,case = False)]
 	outlen = len(matchTicker)
-	
+
 	while(outlen <= 1):
 		print("No result found. Please try again ...")
 		companyTicker = input("Enter the company ticker you want to search:")
@@ -84,16 +84,16 @@ def ticker():
 	while(outlen > 20):
 		print("The matching patterns are more than 20. Please enter more characters...")
 		companyTicker = input("Enter the company ticker you want to search:")
-		matchTicker = companylist[companylist['Symbol'].str.match(companyTicker,case = False)] 
+		matchTicker = companylist[companylist['Symbol'].str.match(companyTicker,case = False)]
 		outlen = len(matchTicker)
-	
+
 	print(matchTicker)
 	#print(companylist.dtypes)
 	comindex = input("Enter the index number of the company you want:")
 	#TODO - Add code to check if the index is proper
 	print("You selected :")
 	print((companylist.iloc[int(comindex)]))
-	menu2(comindex)
+	menu2(companylist,comindex)
 
 def menu():
 	cls()
@@ -106,11 +106,11 @@ def menu():
 		option = input("Your option: ")
 		if option == '1':
 			name()
-		elif option == '2':	
+		elif option == '2':
 			ticker(),
-		elif option == '3':	
+		elif option == '3':
 			print("Thanks for coming. Please visit again ...")
 			exit()
 		else:
-			print("Wrong option. Try again ...")	
+			print("Wrong option. Try again ...")
 menu()
